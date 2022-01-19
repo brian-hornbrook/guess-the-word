ac4d57fd43ed29d4264f5e8595ba18f31208b329
brian-hornbrook <brian_hornbrook@outlook.com>
2022-01-19 14:07:36 -0600

    Displaying guesses on the screen

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guestedLetters = document.querySelector(".guessed-letters");
+const yourGuesses = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const guessLetters = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -7,6 +7,7 @@ const remainingGuesses = document.querySelector(".remaining span");
 const guessMessage = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 const word = "magnolia";
+const guessedLetters = [];
 
 // update word
 const updateWordInProgress = function () {
@@ -24,4 +25,34 @@ guessButton.addEventListener("click", e => {
     const value = guessLetters.value;
     guessMessage.innerText = value;
     guessLetters.value = "";
+    validateInput(value);
+    console.log(guessedLetters);
 });
+
+// validate input
+const validateInput = function (input) {
+    const acceptedLetter = /[a-z]/
+    if (input === "") {
+        guessMessage.innerText = "You must enter something to guess";
+    } else if (input.length > 1) {
+        guessMessage.innerText = "You can only guess one letter at a time";
+    } else if (!input.match(acceptedLetter)) {
+        guessMessage.innerText = "You must enter a letter";
+    } else {
+        makeGuess(input);
+    }
+};
+
+const makeGuess = function (guess) {
+    let added = true;
+    guessedLetters.forEach(letter => {
+        if (guess.toUpperCase() === letter.toUpperCase()) {
+            guessMessage.innerText = "You already guessed that letter";
+            added = false
+        }
+    });
+    if (added === true || guessedLetters.length === 0) {
+        guessedLetters.push(guess);
+        yourGuesses.innerHTML = guessedLetters.join("&nbsp;&nbsp;&nbsp;&nbsp;");
+    }
+};
